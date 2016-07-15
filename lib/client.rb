1bcc90eeca44e02af469f6fa9419ac745fdde320
Halle Williams <williams.hr@gmail.com>
2016-07-15 09:31:42 -0700

    add singleton method for .all, makes client array empty at first

--- lib/client.rb
+++ lib/client.rb
@@ -5,6 +5,16 @@ class Client
     @name = attributes.fetch(:name)
   end
 
+  define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each() do |client|
+      name = client.fetch("name")
+      clients.push(Client.new({:name => name}))
+    end
+    clients
+  end
+
   define_method(:==) do |another_client|
     self.name().==(another_client.name())
   end
