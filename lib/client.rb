b3dc4cf2ad466240c99822564e9ea2b5eb70c065
Halle Williams <williams.hr@gmail.com>
2016-07-15 09:26:26 -0700

    add spec helper, add working spec and methods for initializing client with name attribute and two clients with same name are the same, add failing spec for .all empty at first

--- lib/client.rb
+++ lib/client.rb
@@ -1,6 +1,11 @@
 class Client
+  attr_reader(:name)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+  end
 
   define_method(:==) do |another_client|
     self.name().==(another_client.name())
   end
-end    
+end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,21 +1,24 @@
-require("rspec")
-require("pg")
-require("client")
-
-DB = PG.connect({:dbname => 'hair_salon_test'})
-
-RSPEC.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM clients *;")
-  end
-end
+require('spec_helper')
 
 describe(Client) do
-  describe ("#==")
+  describe ('#name') do
+    it ('tells you the client name') do
+      test_client = Client.new({:name => 'H. Bag'})
+      expect(test_client.name()).to(eq('H. Bag'))
+    end
+  end
+
+  describe ("#==") do
   it("is the same client if it has the same name") do
     client1 = Client.new({:name => "H. Bag"})
     client2 = Client.new({:name => "H. Bag"})
     expect(client1).to(eq(client2))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Client.all()).to(eq([]))
     end
   end
 end
