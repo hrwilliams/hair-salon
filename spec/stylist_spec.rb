fa31419fe4d7eafaecc301944bc69cad6f782ec2
Halle Williams <williams.hr@gmail.com>
2016-07-15 10:21:31 -0700

    add spec for name, method already created

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -5,5 +5,12 @@ describe(Stylist) do
     it("starts off with no stylists") do
       expect(Stylist.all()).to(eq([]))
     end
+  end  
+
+  describe("#name=") do
+    it("tells you its name") do
+      stylist = Stylist.new({:name => "Tommy RR Barber", :id => nil})
+      expect(stylist.name()).to(eq("Tommy RR Barber"))
+    end
   end
 end
