bb629a8968910a47a21c4ac6cd48d62b48a82295
Halle Williams <williams.hr@gmail.com>
2016-07-15 09:37:38 -0700

    add failing spec for save method to add a client to array of saved clients

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -3,16 +3,8 @@ require('spec_helper')
 describe(Client) do
   describe ('#name') do
     it ('tells you the client name') do
-      test_client = Client.new({:name => 'H. Bag'})
-      expect(test_client.name()).to(eq('H. Bag'))
-    end
-  end
-
-  describe ("#==") do
-  it("is the same client if it has the same name") do
-    client1 = Client.new({:name => "H. Bag"})
-    client2 = Client.new({:name => "H. Bag"})
-    expect(client1).to(eq(client2))
+      test_client = Client.new({:name => 'Hair Bag'})
+      expect(test_client.name()).to(eq('Hair Bag'))
     end
   end
 
@@ -21,4 +13,22 @@ describe(Client) do
       expect(Client.all()).to(eq([]))
     end
   end
+
+  describe("#save") do
+    it("adds a client to the array of saved clients") do
+      test_client = Client.new({:name => "Hair Bag"})
+      test_client.save()
+      expect(Client.all()).to(eq([test_client]))
+    end
+  end
+
+  describe ("#==") do
+  it("is the same client if it has the same name") do
+    client1 = Client.new({:name => "Hair Bag"})
+    client2 = Client.new({:name => "Hair Bag"})
+    expect(client1).to(eq(client2))
+    end
+  end
+
+
 end
