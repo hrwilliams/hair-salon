4c16d6c5a7d22810bb1bcb38fc23dafe047b62e7
Halle Williams <williams.hr@gmail.com>
2016-07-18 00:08:10 -0700

    add more integration specss, etc

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ also_reload("lib/**/*.rb")
 require("pg")
 # require("pry")
 
-DB = PG.connect({:dbname => "hair_salon_test"})
+DB = PG.connect({:dbname => "hair_salon"})
 
 get("/") do
   erb(:index)
@@ -15,20 +15,29 @@ end
 get("/stylists/new") do
   erb(:stylist_form)
 end
-#
-# post("/stylists") do
-#   name = params.fetch("name")
-#   stylist = Stylist.new({:name => name, :id => nil})
-#   stylist.save()
-#   erb(:success)
-# end
-#
-# get('stylists') do
-#   @stylists = Stylist.all()
-#   erb(:stylists)
-# end
-#
-# get("/stylists/:id") do
-#   @stylist = Stylist.find(params.fetch("id")).to_i())
-#   erb(:stylist)
-# end
+
+post("/stylists") do
+  name = params.fetch("name")
+  stylist = Stylist.new({:name => name, :id => nil})
+  stylist.save()
+  erb(:stylist_success)
+end
+
+get('stylists') do
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get("/stylists/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  erb(:stylist)
+end
+
+post("/clients") do
+  name = params.fetch("name")
+  stylist_id = params.fetch("stylist_id").to_i()
+  @stylist = Stylist.find(stylist_id)
+  @client = Client.new({:name => name, :stylist_id => stylist_id})
+  @client.save()
+  erb(:client_success)
+end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -14,24 +14,35 @@ describe('adding a new stylist', {:type => :feature}) do
   end
 end
 
-# describe('viewing all of the stylists', {:type => :feature}) do
-#   it('allows a user to see all of the stylists that have been created') do
-#     stylist = Stylist.new({:name => 'Tommy RR Barber'})
-#     stylist.save()
-#     visit('/')
-#     click_button('View All Stylists')
-#     expect(page).to have_content(stylist.name)
-#   end
-# end
-#
-# describe('seeing details for a single stylist', {:type => :feature}) do
-#   it('allows a user to click a stylist to see the clients and details for it') do
-#     test_stylist = Stylist.new({:name => 'Tommy RR Barber'})
-#     test_stylist.save()
-#     test_client= Client.new({:name => "Hair Bag", :stylist_id => test_stylist.id()})
-#     test_client.save()
-#     visit('/stylists')
-#     click_link(test_stylist.name())
-#     expect(page).to have_content(test_client.name())
-#   end
-# end
+describe('viewing all of the stylists', {:type => :feature}) do
+  it('allows a user to see all of the stylists that have been created') do
+    stylist = Stylist.new({:name => 'Tommy RR Barber'})
+    stylist.save()
+    visit('/')
+    click_button('View All Stylists')
+    expect(page).to have_content(stylist.name)
+  end
+end
+
+describe('seeing details for a single stylist', {:type => :feature}) do
+  it('allows a user to click a stylist to see the clients and details for it') do
+    test_stylist = Stylist.new({:name => 'Tommy RR Barber'})
+    test_stylist.save()
+    test_client= Client.new({:name => "Hair Bag", :stylist_id => test_stylist.id()})
+    test_client.save()
+    visit('/stylists')
+    click_link(test_stylist.name())
+    expect(page).to have_content(test_client.name())
+  end
+end
+
+describe('adding clients to a stylist', {:type => :feature}) do
+  it('allows a user to add a client to a stylist') do
+    test_stylist = Stylist.new({:name => 'Tommy RR Barber'})
+    test_stylist.save()
+    visit("/stylists/#{test_stylist.id()}")
+    fill_in("Name", {:with => "Hair Bag"})
+    click_button("Add client")
+    expect(page).to have_content("Success")
+  end
+end
