5080d2d75f71a840ff7c5d085267f35b86657d25
Halle Williams <williams.hr@gmail.com>
2016-07-27 00:39:46 -0700

    fix client find method, all app working except client crud

--- lib/client.rb
+++ lib/client.rb
@@ -1,9 +1,62 @@
+# class Client
+#   attr_reader(:name, :stylist_id)
+#
+#   define_method(:initialize) do |attributes|
+#     @name = attributes.fetch(:name)
+#     @stylist_id = attributes.fetch(:stylist_id)
+#     @id = attributes[:id] || nil
+#   end
+#
+#   define_singleton_method(:all) do
+#     returned_clients = DB.exec("SELECT * FROM clients;")
+#     clients = []
+#     returned_clients.each() do |client|
+#       name = client.fetch("name")
+#       stylist_id = client.fetch("stylist_id").to_i()
+#       clients.push(Client.new({:name => name, :stylist_id => stylist_id}))
+#     end
+#     clients
+#   end
+#
+#
+#
+#   define_method(:save) do
+#     result = DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{@name}', #{@stylist_id}) RETURNING id;")
+#     @id = result.first().fetch("id").to_i()
+#   end
+#
+#   define_method(:==) do |another_client|
+#     self.name().==(another_client.name()).&(self.stylist_id().==(another_client.stylist_id()))
+#   end
+#
+#   define_singleton_method(:find) do |id|
+#     found_client = nil
+#     Client.all().each() do |client|
+#       if client.id().==(id)
+#         found_client = client
+#       end
+#     end
+#     found_client
+#   end
+#
+#   define_method(:update) do |attributes|
+#    @name = attributes.fetch(:name)
+#    @id = self.id()
+#    DB.exec("UPDATE clients SET name = '#{@name}' WHERE id = #{@id};")
+#  end
+#
+#  define_method(:delete) do
+#     DB.exec("DELETE FROM clients WHERE id = #{self.id()};")
+#   end
+# end
+
 class Client
-  attr_reader(:name, :stylist_id)
+  attr_reader(:name, :stylist_id, :id)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @stylist_id = attributes.fetch(:stylist_id)
+    @id = attributes[:id] || nil
   end
 
   define_singleton_method(:all) do
@@ -12,9 +65,19 @@ class Client
     returned_clients.each() do |client|
       name = client.fetch("name")
       stylist_id = client.fetch("stylist_id").to_i()
-      clients.push(Client.new({:name => name, :stylist_id => stylist_id}))
+      id = client.fetch("id").to_i()
+      clients.push(Client.new({:name => name, :stylist_id => stylist_id, :id => id}))
     end
     clients
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{@name}', #{@stylist_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_client|
+    self.name().==(another_client.name()).&(self.id().==(another_client.id()))
   end
 
   define_singleton_method(:find) do |id|
@@ -27,22 +90,12 @@ class Client
     found_client
   end
 
-  define_method(:save) do
-    result = DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{@name}', #{@stylist_id}) RETURNING id;")
-    @id = result.first().fetch("id").to_i()
-  end
-
-  define_method(:==) do |another_client|
-    self.name().==(another_client.name()).&(self.stylist_id().==(another_client.stylist_id()))
+  define_method(:delete) do
+    DB.exec("DELETE FROM clients WHERE id = #{self.id()}")
   end
 
   define_method(:update) do |attributes|
-   @name = attributes.fetch(:name)
-   @id = self.id()
-   DB.exec("UPDATE clients SET name = '#{@name}' WHERE id = #{@id};")
- end
-
- define_method(:delete) do
-    DB.exec("DELETE FROM clients WHERE id = #{self.id()};")
+    @name = attributes.fetch(:name, @name)
+    DB.exec("UPDATE clients SET name = '#{@name}' WHERE id = #{self.id()}")
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -39,13 +39,22 @@ describe(Client) do
     end
   end
 
-
+  describe(".find") do
+      it "returns a client by its ID" do
+        test_client = Client.new({:name => "Eduardo", :stylist_id => 1, :id => nil})
+        test_client.save()
+        test_client2 = Client.new({:name => "Edu", :stylist_id => 1, :id => nil})
+        test_client2.save()
+        expect(Client.find(test_client2.id())).to(eq(test_client2))
+      end
+    end
+    
 describe("#update") do
     it("lets you update clients in the database") do
-      client = Client.new({:name => "Arnold", :id => nil})
+      client = Client.new({:name => "Arnold", :stylist_id => 1, :id => nil})
       client.save()
       client.update({:name => "Alan"})
-      expect(list.name()).to(eq("Alan"))
+      expect(client.name()).to(eq("Alan"))
     end
   end
 
